eb3109658e0e364934616e2f66f0533c25bbc12a
NikkoBogo <nik_bogdanovo@mail.ru>
2023-08-07 12:17:27 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let i = 0;
+    let result = '';
+    let str = String(n);
+    while (i < str.length) {
+        result = `${str[i]}${result}`;
+        i = i + 1;
+  }
+    return Number(result);
 }
